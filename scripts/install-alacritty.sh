da2a3a5142cd7d1257e9e09f0b1f79ec84d8788f
Tom Schammo <tom@tomschammo.com>
2020-09-10 19:43:35 +0200

    No longer using the snap store to get rustup
    Switched to the official install script to get rustup since the
    snapstore is behaving weirdly on Linux Mint 20

--- scripts/install-alacritty.sh
+++ scripts/install-alacritty.sh
@@ -9,14 +9,8 @@ mkdir -p ~/.config/alacritty/
 # some alacritty dependencies and git to clone the repository
 sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3 git
 
-# install the snapstore to get rustup
-if [ -f /etc/apt/preferences.d/nosnap.pref ] ; then
-    sudo rm /etc/apt/preferences.d/nosnap.pref
-fi
-
-sudo apt install -y snapd && sudo snap install rustup --classic
-
-rustup override set stable && rustup update stable
+# installing rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # clone the repo
 git clone https://github.com/alacritty/alacritty.git
@@ -25,10 +19,6 @@ cd alacritty
 
 # build alacritty and place it into the right directory
 cargo build --release && sudo cp target/release/alacritty /usr/local/bin/
-
-# removing all snap stuff cause it sucks
-sudo apt purge snapd -y
-rm -rf ~/snap
 
 cd ~
 

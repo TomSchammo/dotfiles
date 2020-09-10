948c82b3cff6dbfbe8c2e5dfae8fd8c99af6977f
Tom Schammo <tom@tomschammo.com>
2020-09-10 22:11:43 +0200

    Switching to rustup to stable channel
    Accidentally removed lines that switch rusutp to the proper channel, so
    I fixed that again.

--- scripts/install-alacritty.sh
+++ scripts/install-alacritty.sh
@@ -10,7 +10,7 @@ mkdir -p ~/.config/alacritty/
 sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3 git
 
 # installing rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh && rustup override set stable && rustup update stable
 
 # clone the repo
 git clone https://github.com/alacritty/alacritty.git

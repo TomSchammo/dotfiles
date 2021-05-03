b9ad0f8359cc221c4d9cddf9006d7a6cbf951438
Tom Schammo <tom@tomschammo.com>
2021-05-03 15:23:47 +0200

    Added script for installing zsh and everything that goes with it
    Added script that installs zsh (and git if not already installed),
    clones the plugin repositories and moves everything to where it should
    be.

---
+++ scripts/install-zsh.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+PKG_MNGR=""
+PKG_MNGR_INSTALL=""
+
+detect_pkg_manager() {
+    if [ -x "$(command -v apt)" ]; then
+        PKG_MNGR="apt"
+        PKG_MNGR_INSTALL="apt install -y"
+
+    elif [ -x "$(command -v pacman )" ]; then
+        PKG_MNGR="pacman"
+        PKG_MNGR_INSTALL="pacman -S --noconfirm"
+
+    else
+        echo -e "No package manager could be detected on the system\nExiting script..."
+        exit 1
+    fi
+
+}
+
+detect_pkg_manager
+
+echo "Using ${PKG_MNGR} as the system package manager..."
+
+echo "This script needs root privileges to install packages..."
+
+# installing zsh
+sudo ${PKG_MNGR_INSTALL} zsh
+
+# installing git to clone plugins
+sudo ${PKG_MNGR_INSTALL} git
+
+mv dotfiles/zshrc ~/.zshrc
+
+CURRENT_IDR=${pwd}
+
+mkdir -p ~/.config/zsh/plugins
+
+cd ~/.config/zsh/plugins
+
+git clone https://github.com/zsh-users/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions
+
+cd ${CURRENT_IDR}

42e91eaac19507250ca8180779ce6eb09593f7f7
Tom Schammo <tom@tomschammo.com>
2022-09-07 15:40:38 +0200

    feat: Automatically detect package manager
    The install script should automatically find the package manager for
    supported distributions to install packages.
    
    Supported distributions are: openSUSE, fedora, arch (based), debian
    (based)

--- install.sh
+++ install.sh
@@ -2,6 +2,39 @@
 
 # WIP
 
+PKG_MNGR=""
+PKG_MNGR_INSTALL=""
+
+detect_pkg_manager() {
+    if [ -x "$(command -v apt)" ]; then
+        PKG_MNGR="apt"
+        PKG_MNGR_INSTALL="apt install -y"
+
+    elif [ -x "$(command -v pacman )" ]; then
+        PKG_MNGR="pacman"
+        PKG_MNGR_INSTALL="pacman -S --noconfirm"
+
+    elif [ -x "$(command -v dnf )" ]; then
+        PKG_MNGR="dnf"
+        PKG_MNGR_INSTALL="dnf install -y"
+
+    elif [ -x "$(command -v zypper )" ]; then
+        PKG_MNGR="zypper"
+        PKG_MNGR_INSTALL="zypper -n install"
+
+    else
+        echo -e "No package manager could be detected on the system\nExiting script..."
+        exit 1
+    fi
+
+}
+
+detect_pkg_manager
+
+echo "Using ${PKG_MNGR} to install packages..."
+
+echo "This script needs root privileges to install packages..."
+
 packages="zsh rofi nvim \
     alacritty ncmpcpp mpd \
     zathura tmux"

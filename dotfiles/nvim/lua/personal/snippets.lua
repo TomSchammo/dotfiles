7ebb610a8ddf83a152dd6b09c8c2fd682fc48442
Tom Schammo <tom@tomschammo.com>
2023-01-30 16:50:59 +0100

    feat: Load custom snippets
    Load custom vs-code style snippets and autoformat config file

--- dotfiles/nvim/lua/personal/snippets.lua
+++ dotfiles/nvim/lua/personal/snippets.lua
@@ -1,9 +1,10 @@
-
--- support for vscode style snippets like friendly-snips
+-- load friendly-snips
 require("luasnip.loaders.from_vscode").lazy_load()
 
-vim.keymap.set("s", "<C-l>", "<cmd>lua require('luasnip').jump(1)<cr>", {noremap=true, silent=true})
-vim.keymap.set("s", "<C-k>", "<cmd>lua require('luasnip').jump(-1)<cr>", {noremap=true, silent=true})
-vim.keymap.set("i", "<C-l>", "<cmd>lua require('luasnip').jump(1)<cr>", {noremap=true, silent=true})
-vim.keymap.set("i", "<C-k>", "<cmd>lua require('luasnip').jump(-1)<cr>", {noremap=true, silent=true})
+-- load custom vs-code style snippets
+require("luasnip.loaders.from_vscode").load({ paths = { "~/.config/nvim/custom-snippets" } })
 
+vim.keymap.set("s", "<C-l>", "<cmd>lua require('luasnip').jump(1)<cr>", { noremap = true, silent = true })
+vim.keymap.set("s", "<C-k>", "<cmd>lua require('luasnip').jump(-1)<cr>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-l>", "<cmd>lua require('luasnip').jump(1)<cr>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-k>", "<cmd>lua require('luasnip').jump(-1)<cr>", { noremap = true, silent = true })

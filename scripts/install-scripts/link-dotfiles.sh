ee5b646c7ca6722d177ce978384f43e8b4b2d07b
Tom Schammo <tom@tomschammo.com>
2023-02-09 10:44:55 +0100

    fix: Add double quotes to prevent globbing and word splitting
    And autoformat

--- scripts/install-scripts/link-dotfiles.sh
+++ scripts/install-scripts/link-dotfiles.sh
@@ -17,44 +17,44 @@ dotfiles=${git_base}/dotfiles
 # mkdir -p ${XDG_CONFIG_HOME}/{tmux,alacritty,nvim,zsh,zathura,mpd,ncmpcpp}
 
 echo "Linking zshrc..."
-ln -s ${dotfiles}/zshrc ${HOME}/.zshrc &&
-echo "success!" || echo "error when linking zshrc..."
+ln -s "${dotfiles}"/zshrc "${HOME}"/.zshrc &&
+    echo "success!" || echo "error when linking zshrc..."
 
 echo "Linking zsh plugins..."
-ln -s ${dotfiles}/zsh/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking zsh plugins..."
+ln -s "${dotfiles}"/zsh/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking zsh plugins..."
 
 echo "Linking tmux config..."
-ln -s ${dotfiles}/tmux/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking tmux config..."
+ln -s "${dotfiles}"/tmux/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking tmux config..."
 
 echo "Linking alacritty config..."
-ln -s ${dotfiles}/alacritty/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking alacritty config..."
+ln -s "${dotfiles}"/alacritty/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking alacritty config..."
 
 echo "Linking neovim config..."
-ln -s ${dotfiles}/nvim/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking neovim config..."
+ln -s "${dotfiles}"/nvim/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking neovim config..."
 
 echo "Linking zathurarc..."
-ln -s ${dotfiles}/zathura/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking zathurarc..."
+ln -s "${dotfiles}"/zathura/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking zathurarc..."
 
 echo "Linking mpd config..."
-ln -s ${dotfiles}/mpd/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking mpd config..."
+ln -s "${dotfiles}"/mpd/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking mpd config..."
 
 echo "Linking ncmpcpp config..."
-ln -s ${dotfiles}/ncmpcpp/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking ncmpcpp config..."
+ln -s "${dotfiles}"/ncmpcpp/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking ncmpcpp config..."
 
 echo "Linking rofi config..."
-ln -s ${dotfiles}/rofi/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking rofi config..."
+ln -s "${dotfiles}"/rofi/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking rofi config..."
 
 echo "Linking fusuma config..."
-ln -s ${dotfiles}/fusuma/ ${XDG_CONFIG_HOME} &&
-echo "success!" || echo "error when linking fusuma config..."
+ln -s "${dotfiles}"/fusuma/ "${XDG_CONFIG_HOME}" &&
+    echo "success!" || echo "error when linking fusuma config..."
 
 # returning back to the original directory
-cd ${current_dir}
+cd "${current_dir}"

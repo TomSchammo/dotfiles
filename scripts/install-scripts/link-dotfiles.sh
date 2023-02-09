44fc7ff7f529b3e00e53e6f23d6a983a71a645c4
Tom Schammo <tom@tomschammo.com>
2023-02-09 10:59:50 +0100

    feat(): Add color coded messages

--- scripts/install-scripts/link-dotfiles.sh
+++ scripts/install-scripts/link-dotfiles.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# define color codes
+ERROR='\033[0;31m'
+SUCCESS='\033[0;32m'
+NC='\033[0m'
+
 # This scrip links the dotfiles from the repository into their respective place.
 # Symbolic links are created instead of moving or copying the files since it
 # seems kind of silly to have duplicates around when version control exists.
@@ -18,43 +23,43 @@ dotfiles=${git_base}/dotfiles
 
 echo "Linking zshrc..."
 ln -s "${dotfiles}"/zshrc "${HOME}"/.zshrc &&
-    echo "success!" || echo "error when linking zshrc..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking zshrc...${NC}"
 
 echo "Linking zsh plugins..."
 ln -s "${dotfiles}"/zsh/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking zsh plugins..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking zsh plugins...${NC}"
 
 echo "Linking tmux config..."
 ln -s "${dotfiles}"/tmux/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking tmux config..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking tmux config...${NC}"
 
 echo "Linking alacritty config..."
 ln -s "${dotfiles}"/alacritty/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking alacritty config..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking alacritty config...${NC}"
 
 echo "Linking neovim config..."
 ln -s "${dotfiles}"/nvim/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking neovim config..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking neovim config...${NC}"
 
 echo "Linking zathurarc..."
 ln -s "${dotfiles}"/zathura/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking zathurarc..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking zathurarc...${NC}"
 
 echo "Linking mpd config..."
 ln -s "${dotfiles}"/mpd/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking mpd config..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking mpd config...${NC}"
 
 echo "Linking ncmpcpp config..."
 ln -s "${dotfiles}"/ncmpcpp/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking ncmpcpp config..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking ncmpcpp config...${NC}"
 
 echo "Linking rofi config..."
 ln -s "${dotfiles}"/rofi/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking rofi config..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking rofi config...${NC}"
 
 echo "Linking fusuma config..."
 ln -s "${dotfiles}"/fusuma/ "${XDG_CONFIG_HOME}" &&
-    echo "success!" || echo "error when linking fusuma config..."
+    echo "${SUCCESS}success!${NC}" || echo "${ERROR}error when linking fusuma config...${NC}"
 
 # returning back to the original directory
 cd "${current_dir}"

06f923e26866d1e1898aa2a3b10cecd10c8df685
Tom Schammo <tom@tomschammo.com>
2023-12-24 20:20:04 +0100

    feat: Update treesitter

--- dotfiles/nvim/lua/personal/treesitter.lua
+++ dotfiles/nvim/lua/personal/treesitter.lua
@@ -27,8 +27,12 @@ require("nvim-treesitter.configs").setup({
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+
     -- List of parsers to ignore installing
-    -- ignore_install = { "javascript" },
+    ignore_install = {},
 
     highlight = {
         -- `false` will disable the whole extension
@@ -41,6 +45,7 @@ require("nvim-treesitter.configs").setup({
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
+        -- additional_vim_regex_highlighting = { "markdown" },
         additional_vim_regex_highlighting = false,
     },
 

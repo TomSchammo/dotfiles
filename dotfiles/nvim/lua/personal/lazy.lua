048de06ba453174952dac41ccb1641eddbd60e52
Tom Schammo <tom@tomschammo.com>
2024-01-22 18:04:49 +0100

    feat(obsidian): Change how notes are created by `obsidian.nvim`
    1. Remove timestamp from note id
    2. 'Encode' 'Umlaute' (öäü -> oe, ae, ue)
    3. Change ß to ss

--- dotfiles/nvim/lua/personal/lazy.lua
+++ dotfiles/nvim/lua/personal/lazy.lua
@@ -189,14 +189,21 @@ local plugins = {
                 local suffix = ""
                 if title ~= nil then
                     -- If title is given, transform it into valid file name.
-                    suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+                    suffix = title
+                        :gsub(" ", "-")
+                        :gsub("ö", "oe")
+                        :gsub("ä", "ae")
+                        :gsub("ü", "ue")
+                        :gsub("ß", "ss")
+                        :gsub("[^A-Za-z0-9-]", "")
+                        :lower()
                 else
                     -- If title is nil, just add 4 random uppercase letters to the suffix.
                     for _ = 1, 4 do
                         suffix = suffix .. string.char(math.random(65, 90))
                     end
                 end
-                return tostring(os.time()) .. "-" .. suffix
+                return suffix
             end,
 
             -- Optional, customize the backlinks interface.

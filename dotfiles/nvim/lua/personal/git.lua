0f74378bddcd2512562df7e4dc438925cbab21b1
Tom Schammo <tom@tomschammo.com>
2025-03-27 19:03:11 +0100

    feat: Add shortcuts for `git add` and `git add -p`

--- dotfiles/nvim/lua/personal/git.lua
+++ dotfiles/nvim/lua/personal/git.lua
@@ -8,5 +8,7 @@ return {
         { "<leader>gd", "<cmd>Gdiffsplit<cr>", mode = { "n" }, desc = "Open `git diff` in split window" },
         { "<leader>gb", "<cmd>Git blame<cr>", mode = { "n" }, desc = "Open `git blame` in a small split window" },
         { "<leader>gs", "<cmd>G<cr>", mode = { "n" }, desc = "Open vim-fugitive git overview." },
+        { "<leader>ga", "<cmd>Git add %<cr>", mode = { "n" }, desc = "`git add` current file." },
+        { "<leader>gp", "<cmd>Git add -p %<cr>", mode = { "n" }, desc = "`git add -p` current file." },
     },
 }
